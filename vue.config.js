a272ac90a869e0e41d62ceac99fe3ad70de06d4e
moeinZ <moein459@gmail.com>
2022-06-07 22:55:28 +0430

    use dist folder as gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: config => {
-    config.module.rule('').test(/\.fbx$/)
-        .use('file-loader')
-  }
+  publicPath: '/vue-trois-sample/',
 })
